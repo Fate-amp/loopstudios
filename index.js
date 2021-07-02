159540a3edc343eaac3e212eb7ba66c6d07597e1
Fate-amp <84271465+Fate-amp@users.noreply.github.com>
2021-07-03 01:27:02 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+$(".navbar-toggler").on("click",function(){
+    $("nav").addClass('bg-dark');
+    $(".menu-options").addClass('menu-options-dropdown');
+    $("#navbarTogglerDemo02").css("padding","40px 40px");
+    $("nav").css("z-index","1");
+}
+);
